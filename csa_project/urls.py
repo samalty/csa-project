ca4380fc25b6ec1cba2b8265e04dd7b99b33d766
Sam Alty <sam_alty@outlook.com>
2019-08-15 18:58:32 +0100

    More CSS updates

--- csa_project/urls.py
+++ csa_project/urls.py
@@ -17,6 +17,7 @@ from django.contrib import admin
 from django.contrib.auth import views as auth_views
 from django.urls import path, include
 from django.conf import settings
+from .settings import MEDIA_ROOT
 from django.conf.urls.static import static
 
 urlpatterns = [
