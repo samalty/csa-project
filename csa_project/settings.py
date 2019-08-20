69f385bc6b5eaa5b59f95ab56f8f57d54e023331
Sam Alty <sam_alty@outlook.com>
2019-08-20 15:45:07 +0100

    Updated gitignore and secret key in settings

--- csa_project/settings.py
+++ csa_project/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+import env
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8b(np4cx#oli)%cfi&e_ha6z!dl#oc6z99$y1obibckya3day2'
+#SECRET_KEY = '8b(np4cx#oli)%cfi&e_ha6z!dl#oc6z99$y1obibckya3day2'
+
+SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
