4a61b0cefbb90cfd2a7bde0821b8229e07790aa4
Sam Alty <sam_alty@outlook.com>
2019-08-22 11:28:01 +0100

    Updated aws bucket name

--- csa_project/settings.py
+++ csa_project/settings.py
@@ -146,10 +146,11 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_BUCKET_NAME = os.environ.get('AWS_BUCKET_NAME')
 AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 S3_USE_SIGV4 = os.environ.get('S3_USE_SIGV4')
+
+AWS_STORAGE_BUCKET_NAME = 'acceler8-company-logos'
+AWS_BUCKET_NAME = 'acceler8-company-logos'
 
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
