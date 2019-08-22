0bbaf602f1d97ab4295a1f7dd4068e7ad73d91ad
Sam Alty <sam_alty@outlook.com>
2019-08-22 13:37:47 +0100

    Updated S3 bucket settings

--- csa_project/settings.py
+++ csa_project/settings.py
@@ -146,14 +146,14 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+#AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+
+AWS_S3_REGION_NAME = 'eu-west-1'
 
 AWS_STORAGE_BUCKET_NAME = 'acceler8-company-logos'
 AWS_BUCKET_NAME = 'acceler8-company-logos'
 
 AWS_S3_SIGNATURE_VERSION = 's3v4'
-
-AWS_QUERYSTRING_AUTH = False
 
 S3_USE_SIGV4 = True
 AWS_S3_FILE_OVERWRITE = False
