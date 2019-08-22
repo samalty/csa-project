db7bf606a3b9658f69cfebaa191ff3c9d87db8fa
Sam Alty <sam_alty@outlook.com>
2019-08-22 13:39:48 +0100

    Updated S3 bucket settings

--- csa_project/settings.py
+++ csa_project/settings.py
@@ -148,7 +148,7 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 #AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 
-AWS_S3_REGION_NAME = 'eu-west-1'
+AWS_S3_REGION_NAME = 'eu-west-2'
 
 AWS_STORAGE_BUCKET_NAME = 'acceler8-company-logos'
 AWS_BUCKET_NAME = 'acceler8-company-logos'
