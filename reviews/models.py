bfa1457620782f165ed5f5f9fbef5ad73d8cb053
Sam Alty <sam_alty@outlook.com>
2019-08-22 18:14:21 +0100

    Amended review model

--- reviews/models.py
+++ reviews/models.py
@@ -38,67 +38,51 @@ class Accelerator(models.Model):
     @property
     def avg_mentorship(self):
         quantity = Review.objects.filter(subject=self)
-        if len(quantity) > 0:
-            mentorship_result = Review.objects.filter(subject=self).aggregate(avg_mentorship=Avg('mentorship'))['avg_mentorship']
-        else:
-            mentorship_result = 0
-        #mentorship_float = float(mentorship_result)
-        return mentorship_result
+        mentorship_result = Review.objects.filter(subject=self).aggregate(avg_mentorship=Avg('mentorship'))['avg_mentorship']
+        return mentorship_result if len(quantity) > 0 else 0
     
     @property
     def avg_hiring(self):
         quantity = Review.objects.filter(subject=self)
-        if len(quantity) > 0:
-            hiring_result = Review.objects.filter(subject=self).aggregate(avg_hiring=Avg('hiring'))['avg_hiring']
-        else:
-            hiring_result = 0
-        return hiring_result
+        hiring_result = Review.objects.filter(subject=self).aggregate(avg_hiring=Avg('hiring'))['avg_hiring']
+        return hiring_result if len(quantity) > 0 else 0
     
     @property
     def avg_community(self):
         quantity = Review.objects.filter(subject=self)
-        if len(quantity) > 0:
-            community_result = Review.objects.filter(subject=self).aggregate(avg_community=Avg('community'))['avg_community']
-        else:
-            community_result = 0
-        return community_result
+        community_result = Review.objects.filter(subject=self).aggregate(avg_community=Avg('community'))['avg_community']
+        return community_result if len(quantity) > 0 else 0
     
     @property
     def avg_fundraising(self):
         quantity = Review.objects.filter(subject=self)
-        if len(quantity) > 0:
-            fundraising_result = Review.objects.filter(subject=self).aggregate(avg_fundraising=Avg('fundraising'))['avg_fundraising']
-        else:
-            fundraising_result = 0
-        return fundraising_result
+        fundraising_result = Review.objects.filter(subject=self).aggregate(avg_fundraising=Avg('fundraising'))['avg_fundraising']
+        return fundraising_result if len(quantity) > 0 else 0
     
     @property
     def avg_corporate(self):
         quantity = Review.objects.filter(subject=self)
-        if len(quantity) > 0:
-            corporate_result = Review.objects.filter(subject=self).aggregate(avg_corporate=Avg('corporate'))['avg_corporate']
-        else:
-            corporate_result = 0
-        return corporate_result
+        corporate_result = Review.objects.filter(subject=self).aggregate(avg_corporate=Avg('corporate_dev'))['avg_corporate']
+        return corporate_result if len(quantity) > 0 else 0
 
 class Review(models.Model):
     RATINGS = (
-        ('1', '1'),
-        ('2', '2'),
-        ('3', '3'),
-        ('4', '4'),
-        ('5', '5'),
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
     )
     subject = models.ForeignKey(Accelerator, on_delete=models.CASCADE, blank=False)
     title = models.CharField(max_length=200, blank=False)
     author = models.ForeignKey(User, on_delete=models.CASCADE, blank=False)
     feedback = models.TextField(blank=False)
     date_posted = models.DateTimeField(default=timezone.now)
-    mentorship = models.CharField(choices=RATINGS, blank=False, max_length=1, default='1')
-    hiring = models.CharField(choices=RATINGS, blank=False, max_length=1, default='1')
-    community = models.CharField(choices=RATINGS, blank=False, max_length=1, default='1')
-    fundraising = models.CharField(choices=RATINGS, blank=False, max_length=1, default='1')
-    corporate_dev = models.CharField(choices=RATINGS, blank=False, max_length=1, default='1')
+    mentorship = models.IntegerField(choices=RATINGS, blank=False, max_length=1, default='1')
+    hiring = models.IntegerField(choices=RATINGS, blank=False, max_length=1, default='1')
+    community = models.IntegerField(choices=RATINGS, blank=False, max_length=1, default='1')
+    fundraising = models.IntegerField(choices=RATINGS, blank=False, max_length=1, default='1')
+    corporate_dev = models.IntegerField(choices=RATINGS, blank=False, max_length=1, default='1')
     overall = models.DecimalField(decimal_places=2, max_digits=3)
 
     def __str__(self):
@@ -108,12 +92,6 @@ class Review(models.Model):
         return reverse('review_detail', kwargs={'pk': self.pk})
 
     def save(self, *args, **kwargs):
-        # Convert string values to int's for avg function
-        self.mentorship = int(self.mentorship)
-        self.hiring = int(self.hiring)
-        self.community = int(self.community)
-        self.fundraising = int(self.fundraising)
-        self.corporate_dev = int(self.corporate_dev)
         # Set the overall field when the model is saved
         self.overall = (int(self.mentorship) + int(self.hiring) + int(self.community) + \
             int(self.fundraising) + int(self.corporate_dev)) / 5
