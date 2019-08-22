f97b9f7db52fc3ef3186fdcf95863422b84c7f41
Sam Alty <sam_alty@outlook.com>
2019-08-22 16:30:27 +0100

    Added print command to views

--- accelerators/views.py
+++ accelerators/views.py
@@ -19,6 +19,7 @@ def accelerator_detail(request, pk):
         'accelerator': accelerator,
         'reviews': reviews,
     }
+    print(accelerator.avg_mentorship)
     return render(request, 'accelerators/accelerator_detail.html', context)
 
 def accelerator_reviews(request, pk):
