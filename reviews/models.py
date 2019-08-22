6c998e8f21305e573741c86b6ca5c0a6beabd76f
Sam Alty <sam_alty@outlook.com>
2019-08-22 15:31:37 +0100

    Updated avg functions in accelerator model

--- reviews/models.py
+++ reviews/models.py
@@ -39,7 +39,7 @@ class Accelerator(models.Model):
     def avg_mentorship(self):
         quantity = Review.objects.filter(subject=self)
         if len(quantity) > 0:
-            mentorship_result = Review.objects.filter(subject=self).float(aggregate(avg_mentorship=Avg('mentorship'))['avg_mentorship'])
+            float(mentorship_result) = Review.objects.filter(subject=self).aggregate(avg_mentorship=Avg('mentorship'))['avg_mentorship']
         else:
             mentorship_result = 0
         #mentorship_float = float(mentorship_result)
