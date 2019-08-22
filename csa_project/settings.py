20f1b118bc8e8a5b116fd139b47f7354d80f21ee
Sam Alty <sam_alty@outlook.com>
2019-08-22 12:12:08 +0100

    Updated S3 bucket settings

--- csa_project/settings.py
+++ csa_project/settings.py
@@ -151,7 +151,7 @@ AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
 AWS_STORAGE_BUCKET_NAME = 'acceler8-company-logos'
 AWS_BUCKET_NAME = 'acceler8-company-logos'
 
-S3_USE_SIGV4 = True
+#S3_USE_SIGV4 = True
 AWS_S3_FILE_OVERWRITE = False
 AWS_DEFAULT_ACL = None
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
